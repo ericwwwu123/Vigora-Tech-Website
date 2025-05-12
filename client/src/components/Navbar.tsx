13027af212ef0345a0cac1c108e259982435435d
ericwwwu123 <wufan9702@gmail.com>
2025-05-12 13:03:12 -0400

    调整 logo 大小，确保不变形

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -52,7 +52,7 @@ export default function Navbar() {
             }}
             className="flex items-center"
           >
-            <img src={vigoraLogo} alt="Vigora Tech Logo" className="h-12 w-auto object-contain" />
+            <img src={vigoraLogo} alt="Vigora Tech Logo" className="h-16 w-auto object-contain" />
           </a>
           
           {/* Desktop Menu */}
